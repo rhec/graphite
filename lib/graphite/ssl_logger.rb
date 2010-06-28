63b781c1ec285ec8fc294897dc3559acce912685
Robert Eanes <github@robsinbox.com>
2010-06-28 09:14:31 -0500

    Add logger subclass that supports SSL.

---
+++ lib/graphite/ssl_logger.rb
@@ -0,0 +1,26 @@
+require 'openssl'
+
+module Graphite
+  class SslLogger < Logger
+
+    def initialize(server_host, logger = nil, cert = nil, key = nil)
+      super(server_host, logger)
+      @cert = cert
+      @key = key
+    end
+        
+    def socket
+      plain_socket = super()
+      ssl_context = OpenSSL::SSL::SSLContext.new()
+      if @cert && @key
+        ssl_context.cert = OpenSSL::X509::Certificate.new(@cert)
+        ssl_context.key = OpenSSL::PKey::RSA.new(@key)
+      end
+      @socket = OpenSSL::SSL::SSLSocket.new(plain_socket, ssl_context)
+      @socket.sync_close = true
+      @socket.connect 
+      @socket
+    end
+    
+  end
+end
